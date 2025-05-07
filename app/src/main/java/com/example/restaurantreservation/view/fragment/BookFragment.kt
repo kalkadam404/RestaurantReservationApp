a240f700984008364617c9fda46eb02451f1ff9f
Abukhanifa <a_kuatuly@kbtu.kz>
2025-05-07 12:39:58 +0500

    small changes

--- app/src/main/java/com/example/restaurantreservation/view/fragment/BookFragment.kt
+++ app/src/main/java/com/example/restaurantreservation/view/fragment/BookFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
